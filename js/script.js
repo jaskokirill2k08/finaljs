62637c1ff862d25eae89929cee171838cd121875
jaskokirill2k08 <55052948+jaskokirill2k08@users.noreply.github.com>
2021-09-26 14:17:53 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+﻿// console.log('Hi!');
+function changeVideo(url){
+    document.getElementById('video-slider').src = url;
+}
+
+//вкладка смена информации при потере активности
+
+let timer;
+let t = document.querySelector('head title');
+
+function changeTitle(icon, text) {
+    document.querySelector('head title').innerHTML = text;
+    document.querySelector('link[rel="icon"]').setAttribute('href', icon);
+}
+
+window.onblur = () => {
+    timer = setTimeout(() => {
+        changeTitle('./heart.ico', 'Вернитесь...');
+    }, 3000)
+}
+
+window.onfocus = () => {
+    clearTimeout(timer);
+    changeTitle('./favicon.ico', t);
+}
